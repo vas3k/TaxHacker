b47dc4b80a68cfb8184c28c8d07686b54df9863b
Vasily Zubarev <me@vas3k.ru>
2025-04-08 20:56:23 +0200

    feat: add sentry bug reporting

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withSentryConfig } from "@sentry/nextjs"
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
@@ -11,4 +12,15 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+const isSentryEnabled = process.env.NEXT_PUBLIC_SENTRY_DSN && process.env.SENTRY_ORG && process.env.SENTRY_PROJECT
+
+export default isSentryEnabled
+  ? withSentryConfig(nextConfig, {
+      silent: !process.env.CI,
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      disableLogger: true,
+      widenClientFileUpload: true,
+      tunnelRoute: "/monitoring",
+    })
+  : nextConfig
